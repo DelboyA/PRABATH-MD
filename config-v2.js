12a19668a1f13bd62d9e87bd951a23cfaeb2e3e4
DJ DELBOY ANIEY <168739965+DelboyA@users.noreply.github.com>
2024-08-04 07:51:15 +0200

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~snMU1K7D#54wZwlpIqojJ4ip5e9zkSTElS1LYx-uRkwKYQCPxAec"
 
 
 module.exports = {
